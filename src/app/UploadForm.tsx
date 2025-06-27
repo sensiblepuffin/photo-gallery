b7f2534fab33d1387764859bf10d0a5a9867c8b8
sensiblepuffin <175900+sensiblepuffin@users.noreply.github.com>
2025-06-27 13:20:23 -0400

    Add quota exceeded notification

--- src/app/UploadForm.tsx
+++ src/app/UploadForm.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import { Add } from "@mui/icons-material";
-import { Box, IconButton } from "@mui/material";
-import { useRef } from "react";
+import { Box, IconButton, Snackbar } from "@mui/material";
+import { useRef, useState } from "react";
 
 export const UploadForm = () => {
   const imgRef = useRef(null);
+  const [storageExceeded, setStorageExceeded] = useState(false);
 
   const onChangeFile = (evt) => {
     let selected = evt.target.files;
@@ -15,14 +16,20 @@ export const UploadForm = () => {
       const reader = new FileReader();
 
       reader.addEventListener('load', () => {
-        newPhotos = newPhotos.concat([reader.result.toString()]);
-        // console.info('UploadForm: new', newPhotos);
-        localStorage.setItem("photos", JSON.stringify(photos.concat(newPhotos)));
-        window.dispatchEvent(
-          new StorageEvent('storage', 
-            { key: 'photos', newValue: reader.result.toString() }
-          )
-        );
+        try {
+          newPhotos = newPhotos.concat([reader.result.toString()]);
+          // console.info('UploadForm: new', newPhotos);
+          localStorage.setItem("photos", JSON.stringify(photos.concat(newPhotos)));
+          window.dispatchEvent(
+            new StorageEvent('storage',
+              { key: 'photos', newValue: reader.result.toString() }
+            )
+          );
+        }
+        catch (error) {
+          console.warn('Quota Exceeded');
+          setStorageExceeded(true);
+        }
       }, false);
 
       if (selected.item(i)) {
@@ -43,6 +50,13 @@ export const UploadForm = () => {
         multiple
         style={{ display: 'none' }}
         onChange={onChangeFile}
+      />
+      <Snackbar
+        open={storageExceeded}
+        autoHideDuration={5000}
+        onClose={() => setStorageExceeded(false)}
+        message="Storage capacity exceeded. Delete some photos first."
+        sx={{ left: 'inherit', right: '40px', bottom: '80px', }}
       />
       <IconButton sx={{ width: '20px', height: '20px' }} onClick={handleClick}>
         <Add color="primary" sx={{ fontSize: '48px', border: 'solid', borderRadius: '5px', borderWidth: '2px' }} />
