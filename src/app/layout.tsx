976cb9487c6fcc7657e606ff9fed93a3bf33a783
sensiblepuffin <175900+sensiblepuffin@users.noreply.github.com>
2025-06-27 13:20:11 -0400

    Fixed icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/Photo.ico" sizes="any" />
       <body>
         <main>{children}</main>
       </body>
